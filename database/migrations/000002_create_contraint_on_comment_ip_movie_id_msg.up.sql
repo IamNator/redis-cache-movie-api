0af17a54147327b37df8f9cb89314c24fe3a03cb
Nator Verinumbe <natverior1@gmail.com>
2023-03-08 19:31:15 +0100

    fix: migration files

--- database/migrations/000002_create_contraint_on_comment_ip_movie_id_msg.up.sql
+++ database/migrations/000002_create_contraint_on_comment_ip_movie_id_msg.up.sql
@@ -1,2 +1,2 @@
 CREATE UNIQUE INDEX IF NOT EXISTS comment_swapi_movie_id_message_ipv4_addr_uindex
-    ON busha.public.comment(swapi_movie_id, message, ipv4_addr);
+    ON comment(swapi_movie_id, message, ipv4_addr);
